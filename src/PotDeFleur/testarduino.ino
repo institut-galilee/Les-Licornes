0f18eb0abf3d25a2b940c667535a13bec52867c8
leodechaumet <56650960+leodechaumet@users.noreply.github.com>
2020-01-08 17:00:25 +0100

    Update testarduino.ino

--- src/PotDeFleur/testarduino.ino
+++ src/PotDeFleur/testarduino.ino
@@ -1,63 +1,66 @@
+
+#include <ArduinoJson.h>
+#include <HTTPClient.h>
 #include <WiFi.h>
-const char* ssid = "TEST";
-//const char* password = NULL;
+
+
+// Set these to run example.
+#define FIREBASE_HOST "https://careplant-bad6a.firebaseio.com"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+#define BD_TEMP "arduino/capteurs/temperature"
+#define BD_NIVEAU "arduino/capteurs/niveau"
+#define BD_HUM "arduino/capteurs/humidite"
+#define BD_TIMESTAMP "arduino/timestamp"
+
+#define BD_HUM_PLUS "info/humPlus"
+#define BD_HUM_MOINS "info/humMoins"
+
+int temp = 0;
+int hum = 20;
+int niveau = 3;
+int timestamp = 5;
+
+
 void setup() {
-  Serial.begin(9600);
- // Serial.print("string");
-//  byte b = Serial.read();
-    WiFi.begin(ssid);
+ Serial.begin(115200);
+ Serial.println("Interfacfing arduino with nodemcu");
+ sw.begin(115200);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Establishing connection to WiFi..");
-  }
+}
 
-  Serial.println("Connected to network");
+//liberer mem alloué
+/*char * paramToJson(char * param, char * values, int taille){
+    retour = malloc(sizeof(int) * 100);
+    strcat(retour, "{");
+    strcat(retour, param);
+    strcat(retour, ":");
+    strcat(retour, values);
+    strcat(retour, "}");
 
+}*/
+
+void updateVal(char * param, char * value){
+    DynamicJsonDocument  doc(200);
+    doc[param] = value;
+
+    String output;
+    serializeJsonPretty(doc, output);// You can also use serializeJson(doc, Serial);
+    Serial.println(output);
+    sx.println(output);
+}
+
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+  Serial.println(x);
 }
 
 void loop() {
-scanNetworks();
-printWiFiInfo();
 
-}
-
-
-
-
-
-void scanNetworks() {
-
-  int numberOfNetworks = WiFi.scanNetworks();
-
-  Serial.print("Number of networks found: ");
-  Serial.println(numberOfNetworks);
-
-  for (int i = 0; i < numberOfNetworks; i++) {
-
-    Serial.print("Network name: ");
-    Serial.println(WiFi.SSID(i));
-
-    Serial.print("Signal strength: ");
-    Serial.println(WiFi.RSSI(i));
-
-    Serial.print("MAC address: ");
-    Serial.println(WiFi.BSSIDstr(i));
-
-    //Serial.print("Encryption type: ");
-    //Serial.print(WiFi.encryptionType(i)); the type is  wifi_auth_mode_t
-
-    Serial.println("-----------------------");
-
-  }
-}
-
-
-
-void printWiFiInfo() {
-  Serial.println(WiFi.macAddress());
-  Serial.println(WiFi.localIP());
-
-  //WiFi.disconnect(false);
-  Serial.println(WiFi.localIP());
+    updateVal(BD_TEMP, temp);
+    updateVal(BD_HUM, hum);
+    updateVal(BD_NIVEAU, niveau);
+    updateVal(BD_TIMESTAMP, timestamp);
+    delay(5000)
 }
