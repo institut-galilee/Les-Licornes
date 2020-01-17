ef4d474703440466a7ee9f06747bed33f27168f8
leodechaumet <56650960+leodechaumet@users.noreply.github.com>
2020-01-17 17:10:33 +0100

    Update proxy.py

--- src/PotDeFleur/proxy.py
+++ src/PotDeFleur/proxy.py
@@ -11,9 +11,18 @@ ser.open()
 data = ser.readline()
 splt=data.split()
 
+tempP = 0
+tempM = 0
+humP = 0
+humM = 0
+
 ligne = ''
 val = []
 payload = ''
+
+def sendGetRequest():
+	r = requests.get("https://careplant-bad6a.firebaseio.com/info.json")
+	return json.loads(r.text)
 
 def sendPatchRequest(ligne):
     val = ligne.split(' ')
@@ -21,6 +30,8 @@ def sendPatchRequest(ligne):
     r = requests.patch(URLDB_PATCH, data = payload, headers={"Content-Type": "application/json"})
     if (r.status_code != 200):
         print("Erreur dans l'enregistrement des donnees")
+        
+
 
 
 while True:
